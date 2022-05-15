4b6103c866b09c8f5c686ad4022b15eb7b1e1842
RojinRastegarpour <87577400+RojinRastegarpour@users.noreply.github.com>
2022-05-15 22:28:25 +0430

    Update sysproc.c

--- xv6-public/sysproc.c
+++ xv6-public/sysproc.c
@@ -98,3 +98,63 @@ sys_find_next_prime_number(void)
   cprintf("in kernel systemcall sys_find_next_prime_number() called for number %d\n", a);
   return find_next_prime_number(a);
 }
+
+int sys_change_queue(void)
+{
+  int pid;
+  int dst_queue;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+
+  if(argint(1, &dst_queue) < 0)
+    return -1;
+  
+  change_sched_queue(pid, dst_queue);
+  return 1;
+}
+
+
+int sys_set_ratio_process(void)
+{
+  int pid;
+  int priority_ratio;
+  int arrival_time_ratio;
+  int executed_cycle_ratio;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+
+  if(argint(1, &priority_ratio) < 0)
+    return -1;
+
+  if(argint(2, &arrival_time_ratio) < 0)
+    return -1;
+
+  if(argint(3, &executed_cycle_ratio) < 0)
+    return -1;
+  
+  set_ratio_process(pid, priority_ratio, arrival_time_ratio, executed_cycle_ratio);
+  return 1;
+}
+
+int sys_set_priority(void)
+{
+  int pid;
+  int priority;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+
+  if(argint(1, &priority) < 0)
+    return -1;
+  
+  set_priority(pid, priority);
+  return 1;
+}
+
+int sys_print_processes_details(void)
+{
+  print_processes_details();
+  return 1;
+}
